1d702aec7bc6d80dc3787f0281c1c09093321ff0
Charlis_Dias <83874687+Starteletron@users.noreply.github.com>
2025-05-15 01:35:20 -0300

    Delete Sistema Contábil 16.0 directory

--- Sistema Contábil 16.0/js/geral.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('menu-toggle');
-    const sidebar = document.getElementById('sidebar');
-  
-    toggleButton.addEventListener('click', () => {
-      sidebar.classList.toggle('active');
-    });
-  });
-  
